4439059aa78359b3b8b98772d6ee2b89b82b5fa2
Kevin Reid <kpreid@switchb.org>
2026-05-17 10:20:56 -0700

    Add tested example for `err_is_todo()`.

--- panic-example-test/src/main.rs
+++ panic-example-test/src/main.rs
@@ -1,44 +1,34 @@
 //! Binary used by the `stderr_format` test target to as a way to examine the full formatted
 //! panic output including the Rust parts.
 
-use std::error::Error;
-use std::fmt;
+#![allow(invalid_from_utf8)]
 
-use descriptive_unwrap::{OptionUnwrapExt as _, ResultUnwrapExt as _};
+use descriptive_unwrap::OptionUnwrapExt as _;
 
 fn main() {
     let args_strings: Vec<String> = std::env::args().collect();
     let args_strs: Vec<&str> = args_strings.iter().map(String::as_str).collect();
 
-    let a_result: Result<(), ParseError> = "nan".parse::<i32>().map(drop).map_err(ParseError);
-
     match args_strs[1..] {
         ["Option::none_is_unreachable"] => None.none_is_unreachable(),
         ["Option::none_is_todo"] => None.none_is_todo(),
         ["Result::err_is_unreachable"] => err_is_unreachable_example::run(),
-        ["Result::err_is_todo"] => a_result.err_is_todo(),
+        ["Result::err_is_todo"] => err_is_todo_example::run(),
         _ => panic!("unrecognized subcommand: {args_strs:?}"),
-    }
-}
-
-#[derive(std::fmt::Debug)]
-struct ParseError(std::num::ParseIntError);
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.pad("failed to parse input")
-    }
-}
-
-impl Error for ParseError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(&self.0)
     }
 }
 
 #[allow(unused_variables)]
 mod err_is_unreachable_example {
     include!("../../doc-example-parts/err_is_unreachable.rs");
+    pub fn run() {
+        main()
+    }
+}
+
+#[allow(unused_variables)]
+mod err_is_todo_example {
+    include!("../../doc-example-parts/err_is_todo.rs");
     pub fn run() {
         main()
     }
--- src/lib.rs
+++ src/lib.rs
@@ -73,6 +73,28 @@ pub trait ResultUnwrapExt<T, E> {
     ///
     /// Use this like [`todo!`]:
     /// the code is incomplete and error handling should be added.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use descriptive_unwrap::ResultUnwrapExt as _;
+    ///
+    /// let input = [0x48, 0x65, 0x6c, 0x6c, 0x6f];
+    ///
+    /// let string = std::str::from_utf8(&input).err_is_todo();
+    /// println!("{string}");
+    /// ```
+    ///
+    /// If instead there is an error, it will panic with a message using the `Display` formatting
+    /// of the error value:
+    ///
+    /// ```rust,should_panic
+    #[doc = include_str!("../doc-example-parts/err_is_todo.rs")]
+    /// ```
+    ///
+    /// ```text
+    #[doc = include_str!("../doc-example-parts/err_is_todo.stderr")]
+    /// ```
     fn err_is_todo(self) -> T;
 
     #[doc(hidden)]
